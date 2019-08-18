f9c225d099999734e8c1c67bfd0bd8442a6cbb24
Dominic <dominic.m.chu@gmail.com>
2019-08-18 05:10:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,6 +12,7 @@ const codes = [
 ];
 
 function init() {
-  document.body.addEventListener('keydown', function(e) {
-    
+  document.body.addEventListener('keydown', keyLog(e) {
+    let index = 0;
+    const 
   })
